631caac0930f5d3fdd0822250a416ab0fc5a3d43
DimasCW <cdimas292@gmail.com>
2024-06-24 20:29:04 +0700

    tugas hari 11

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
 import dynamic from  "next/dynamic";
-import Image from "next/image"; 
-import { useEffect } from "react"; 
+import Link from "next/link";
 
 const LayoutComponent = dynamic (() => import("@/layout"))
 
 export default  function Main(){
-  useEffect(()=> {
-    fetch("/api/hello")
-    .then((res) => res.json())
-    .then((res)=> console.log("response =>", res))
-    .catch((err) => console.log("err =>", err));
-  },[]);
 
   return(
     <>
       <LayoutComponent metaTitle="Home">
-        Home
-        <Image src="/icon.png" width={300} height={300} alt="huaa" />
+        <ul>
+          <li>
+            <Link href="/notes">Notes</Link>
+            </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </LayoutComponent>
     </>
 
